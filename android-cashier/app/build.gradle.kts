9784a595858bb5b8d6d897ee6e1a45934581c073
suryausman313-cell <suryausman313@gmail.com>
2026-08-09 18:29:37 +0400

    Add files via upload

--- android-cashier/app/build.gradle.kts
+++ android-cashier/app/build.gradle.kts
@@ -1,1 +1,28 @@
-U
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.mahipos.cashier"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.mahipos.cashier"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+}

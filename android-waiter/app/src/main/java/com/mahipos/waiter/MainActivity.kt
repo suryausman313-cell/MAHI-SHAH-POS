e5285c83f1bc03f9ce6b815da2f3ad073e709e64
suryausman313-cell <suryausman313@gmail.com>
2026-08-10 19:17:32 +0400

    Add files via upload

--- android-waiter/app/src/main/java/com/mahipos/waiter/MainActivity.kt
+++ android-waiter/app/src/main/java/com/mahipos/waiter/MainActivity.kt
@@ -1,1 +1,53 @@
-ffg
+package com.mahipos.waiter
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
+    companion object {
+        private const val APP_URL = "https://mahi-shah-pos.suryausman313.workers.dev/waiter"
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        webView = WebView(this)
+        setContentView(webView)
+
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.databaseEnabled = true
+        webView.settings.allowFileAccess = true
+        webView.settings.allowContentAccess = true
+        webView.settings.setSupportZoom(false)
+
+        webView.webChromeClient = WebChromeClient()
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                request: WebResourceRequest?
+            ): Boolean {
+                return false
+            }
+        }
+
+        webView.loadUrl(APP_URL)
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
